d4569c7bec033abd96593a8f8bd6a2e432294e38
Kevin Mpofu <40406337+webvelocity@users.noreply.github.com>
2020-02-12 15:15:07 +0200

    Update snippets.js

--- snippets.js
+++ snippets.js
@@ -1,9 +1,9 @@
-
-        const el = document.forms["query-form"];
-        el.onsubmit = function () {
-            var emailCookie = document.getElementById("field4").value;
-            document.cookie = `email=${emailCookie}`; //set client cookie
-        }
+//Get form by name
+const el = document.forms["query-form"];
+el.onsubmit = function () {
+var emailCookie = document.getElementById("field4").value;
+document.cookie = `email=${emailCookie}`; //set client cookie
+}
 
 //Setting Cookies
  document.cookie = `email=${emailCookie}`; //set client cookie
