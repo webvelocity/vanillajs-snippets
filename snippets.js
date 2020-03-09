a7ede0bca6e35e4e94c5816b70e190bebdadf579
Kevin Mpofu <40406337+webvelocity@users.noreply.github.com>
2020-03-09 15:33:38 +0200

    Update snippets.js

--- snippets.js
+++ snippets.js
@@ -7,3 +7,20 @@ document.cookie = `email=${emailCookie}`; //set client cookie
 
 //Setting Cookies
  document.cookie = `email=${emailCookie}`; //set client cookie
+
+
+
+//Get absolute URL
+var getAbsoluteUrl = (function() {
+	var a;
+
+	return function(url) {
+		if(!a) a = document.createElement('a');
+		a.href = url;
+
+		return a.href;
+	};
+})();
+
+// Usage
+getAbsoluteUrl('/something'); // https://davidwalsh.name/something
