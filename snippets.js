df1e752cde2249862e53b326e6a9bc177960ccad
Kevin Mpofu <40406337+webvelocity@users.noreply.github.com>
2020-03-20 21:40:37 +0200

    Update snippets.js

--- snippets.js
+++ snippets.js
@@ -24,3 +24,21 @@ var getAbsoluteUrl = (function() {
 
 // Usage
 getAbsoluteUrl('/something'); // https://davidwalsh.name/something
+
+
+
+//Toggle and Close Menu
+
+function toggleHeaderMenu() {
+	if (0 == document.getElementsByClassName("ss-inner-contents-active").length) {
+		document.getElementById("ssMenu").classList.toggle("ss-menu-wrapper-active");
+		for (var e = document.getElementsByClassName("ss-inner-contents"), t = 0; t < e.length; t++) setTimeout(function (e) {
+			e.classList.add("ss-inner-contents-active")
+		}, 325, e[t])
+	} else {
+		for (e = document.getElementsByClassName("ss-inner-contents"), t = 0; t < e.length; t++) e[t].classList.remove("ss-inner-contents-active");
+		setTimeout(function () {
+			document.getElementById("ssMenu").classList.toggle("ss-menu-wrapper-active")
+		}, 325)
+	}
+}
