78c49bbc7c34557a54abee4142b400bf942bc5e3
Kevin Mpofu <40406337+webvelocity@users.noreply.github.com>
2020-11-17 16:18:48 +0200

    Update snippets.js

--- snippets.js
+++ snippets.js
@@ -1,3 +1,20 @@
+    /**
+     * Simulate a click event.
+     * @public
+     * @param {Element} elem  the element to simulate a click on
+     */
+    var simulateClick = function (elem) {
+        // Create our event (with options)
+        var evt = new MouseEvent('click', {
+            bubbles: true,
+            cancelable: true,
+            view: window
+        });
+        // If cancelled, don't dispatch our event
+        var canceled = !elem.dispatchEvent(evt);
+    };
+
+
 //Get form by name
 const el = document.forms["query-form"];
 el.onsubmit = function () {
